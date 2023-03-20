65401127d5932af5abb371779f5c127ed884a4d6
Enya Emmanuel <airnyac@gmail.com>
2023-03-20 06:17:53 +0000

    Prints Base 10 Num. with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char LowNum;
+
+	for (LowNum = '0'; LowNum <= '9'; ++LowNum)
+		putchar(LowNum);
+	putchar('\n');
+	return (0);
+}
