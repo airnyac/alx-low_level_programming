d1f228f7026090fb120188aa70d64ed3a16d35f9
Enya Emmanuel <airnyac@gmail.com>
2023-03-20 13:58:53 +0000

    Iterate Numbers with CommaSpace

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		/*prints the value of i as character*/
+		putchar(i + '0');
+
+		/*Adds a comma and a space to each number*/
+		if (i != 9)
+			putchar(',');
+			putchar(' ');
+	putchar('\n');
+	return (0);
+}
