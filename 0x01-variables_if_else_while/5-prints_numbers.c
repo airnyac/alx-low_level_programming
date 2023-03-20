4e25044b52e1d5614629f97293d04de64d2127f3
Enya Emmanuel <airnyac@gmail.com>
2023-03-20 05:21:41 +0000

    Prints Base 10 numbers

---
+++ 0x01-variables_if_else_while/5-prints_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int LowNum;
+
+	for (LowNum = 0; LowNum < 10; LowNum++)
+		printf("%d", LowNum);
+	printf("\n");
+	return (0);
+}
