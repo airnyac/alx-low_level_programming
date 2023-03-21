8ef6a290f013b0717ab69cc514656cdc99dfba90
Enya Emmanuel <airnyac@gmail.com>
2023-03-21 10:08:24 +0000

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+putchar('\n');
+return (0);
+}
