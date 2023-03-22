68f0646c5ca4a1a946eb26c70cbf166b23bd4d36
Enya Emmanuel <airnyac@gmail.com>
2023-03-22 14:34:31 +0000

    Right on Time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ *
+ * Return: always 0
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 3; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(58);
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar(10);
+				}
+			}
+		}
+	}
+}
