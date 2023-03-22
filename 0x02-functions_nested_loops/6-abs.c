36e7e59d602aeb228f8340c1ae90102c28059e5f
Enya Emmanuel <airnyac@gmail.com>
2023-03-22 13:11:23 +0000

    Prints Absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,22 +8,8 @@
 int _abs(int n)
 {
 	if (n < 0)
-	{
 		n = -n;
-	}
-
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-
-	while (n != 0)
-	{
-		int digit = n % 10;
-		char i = digit + '0';
-
-		_putchar(i);
-		n /= 10;
-	}
-	return n;
+	else if (n >= 0)
+		n = n;
+	return (n);
 }
