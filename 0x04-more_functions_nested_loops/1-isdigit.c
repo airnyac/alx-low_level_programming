f9cad7bf040a446503308684e46ffc4e94520469
Enya Emmanuel <airnyac@gmail.com>
2023-03-23 20:19:36 +0000

    checks for digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - chechk for digit 0 - 9
+ * @c: Stores digit
+ * Return: Always 0
+ */
+int _isdigit(int c)
+{
+
+	if (c == 48 && c <= 57)
+	{
+		return (1);
+	}
+		return (0);
+}
