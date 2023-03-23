4c8ff336e64b07cc7d57fead9ba9b20c64adef47
Enya Emmanuel <airnyac@gmail.com>
2023-03-23 05:02:19 +0000

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - Prints last 52 fib. numbers
+ * Return: void
+ */
+int main(void)
+{
+	int x = 0;
+	long y = 1, z = 2;
+
+	while (x < 50)
+	{
+		if (x == 0)
+		{
+			printf("%ld", y);
+		}
+		else if (x == 0)
+		{
+			printf("%ld", z);
+		}
+		else
+		{
+			z += y;
+			printf("%ld", y);
+		}
+		++x;
+	}
+	printf("\n");
+	return (0);
+}
