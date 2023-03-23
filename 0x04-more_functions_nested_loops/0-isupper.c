7d031ff4fbfa5703dbfe469282ae0028bf69e83a
Enya Emmanuel <airnyac@gmail.com>
2023-03-23 20:00:18 +0000

    Check for uppercase letters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,27 +7,14 @@
  */
 int _isupper(int c)
 {
-	int x;
-	int y;
 
-
-	for (x = 'a'; x <= 'z'; c++)
+	if (c >= 65 && c <= 90)
 	{
-		for (y = 'A'; y <= 'Z'; y++)
-		{
-			if (c == x)
-			{
-				return (0);
-			}
-			else
-			{
-				if (c == y)
-				{
-					return (1);
-				}
-			}
-		}
+		return (1);
 	}
-	return (1);
+	else
+	{
+		return (0);
+	}
 }
 
