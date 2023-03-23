0045a263fafa88160213731e8a9b97738d551dd7
Enya Emmanuel <airnyac@gmail.com>
2023-03-23 03:02:04 +0000

    Funct. that adds a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds the value of two numbers
+ * @a: checks value of a
+ * @b: ckecks value of b
+ * Return: Always a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
