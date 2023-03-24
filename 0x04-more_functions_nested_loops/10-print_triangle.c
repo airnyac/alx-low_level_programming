4a86ce827417230445a376d9a97694a45e128ea1
Enya Emmanuel <airnyac@gmail.com>
2023-03-24 03:03:46 +0000

    Try-angle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print a triangle of Pound wrt parameter
+ * @size: store parameter
+ * Return: Null
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - i; j > 1; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
