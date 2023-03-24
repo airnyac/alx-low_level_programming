e242d796eff5dbc3811597b8ca864c0f92fd2f6a
Enya Emmanuel <airnyac@gmail.com>
2023-03-24 03:09:10 +0000

    prints digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,15 +5,13 @@
  * @c: Stores digit
  * Return: Always 0
  */
+
 int _isdigit(int c)
 {
 
-	if (c == 48 && c <= 57)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
-	else
-	{
 		return (0);
-	}
 }
