bb4fd602529cd09c832ef47fa298d887ae878406
Enya Emmanuel <airnyac@gmail.com>
2023-03-24 03:38:12 +0000

    prints number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: take int to output
+ * Return: Null
+ */
+
+void print_number(int n)
+{
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
+	k /= 10;
+
+	if (k != 10)
+	{
+		print_number(k);
+		_putchar((unsigned int) n % 10 + '0');
+	}
+}
